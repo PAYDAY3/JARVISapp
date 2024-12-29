a53e775cb5f1c2bd84b9fe4c3da3e5c9945ea0a6
PAYDAY3 <1484533583@qq.com>
2024-12-29 12:31:57 +0800

    Create TaskDao.kt

---
+++ Android/app/src/main/assets/com/example/jarvisassistant/data/TaskDao.kt
@@ -0,0 +1,20 @@
+package com.example.jarvisassistant.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks")
+    fun getAllTasks(): LiveData<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+}
+
